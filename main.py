9deca276e333f712db27e8c3f4b490d4b1e2531f
vijay-ss <54510959+vijay-ss@users.noreply.github.com>
2022-12-23 18:50:47 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import os
 import json
 from datetime import datetime
 
-from gcp_utils.upload_to_gcs import upload_blob, credentials_accessor
-from spotify_api.refresh_token import Refresh_Access_Token
+from gcp_utils.common_functions import upload_blob, credentials_accessor
+from spotify_api.auth import SpotifyToken
 from spotify_api.spotify_api import PlaybackHistory
 
 if __name__ == "__main__":
     credentials = credentials_accessor()
     bucket_name = credentials.get('BUCKET_NAME')
+    t = SpotifyToken(credentials.get('REFRESH_TOKEN'), credentials.get('CLIENT_ID'), credentials.get('CLIENT_SECRET'))
+    access_token = t.request_token()
 
-    a = Refresh_Access_Token(credentials.get('REFRESH_TOKEN'), credentials.get('BASE_64'))
-    token = a.request_token()
-
-    api = PlaybackHistory(token)
+    api = PlaybackHistory(access_token)
     j = api.get_playback_history()
 
     track_ids = api.extract_track_id(j)
@@ -20,7 +20,6 @@ if __name__ == "__main__":
 
     j_feat = api.get_track_features(track_ids)
     j_genres = api.get_track_genres(artist_ids)
-    #j_recommend = api.get_recommendations(track_ids)
     
     with open('playback_hist.json', 'w') as f:
      f.write(json.dumps(j, indent=4, sort_keys=True))
