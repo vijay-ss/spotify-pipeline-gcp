7bea561915e8601932c3b4eec55ac7466af0ab6b
Vijay Saddi <vijaysaddi@Vijays-MacBook-Pro.local>
2023-08-20 19:38:21 -0400

    add data extraction scheduler step to cloudbuild.yml

--- gcp_utils/common_functions.py
+++ gcp_utils/common_functions.py
@@ -39,7 +39,9 @@ def get_credentials():
         try:
             client = secretmanager.SecretManagerServiceClient()
             project_number = get_project_number(os.environ.get("PROJECT_ID"))
-            response = client.access_secret_version(request={"name": f"projects/{project_number}/secrets/credentials/versions/latest"})
+            response = client.access_secret_version(
+                request={"name": f"projects/{project_number}/secrets/credentials/versions/latest"}
+            )
             payload = ast.literal_eval(response.payload.data.decode("UTF-8"))
             print("GCP import successful.")
 
@@ -51,14 +53,13 @@ def get_credentials():
 
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-    # The path to your file to upload
-    # source_file_name = "local/path/to/file"
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
+    """Uploads a file to the bucket.
 
+    Args:
+        bucket_name (str): ID of GCS bucket.
+        source_file_name (str): Path to file to upload.
+        destination_blob_name (str): ID of GCS object.
+    """
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
